74f1c98fcced13e16ec7da2fba0b700bdca357fd
dev1peru <34497733+dev1peru@users.noreply.github.com>
2024-02-28 11:37:34 -0500

    add test files
    add test files

---
+++ config_template.py
@@ -0,0 +1,6 @@
+config = dict()
+config['sp_user'] = 'some_user@example.com'
+config['sp_password'] = 'Passw0rd'
+config['sp_base_path'] = 'https://example.sharepoint.com'
+config['sp_site_name'] = 'my_site_name'
+config['sp_doc_library'] = 'PythonUploader'
